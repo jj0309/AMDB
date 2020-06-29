c572022e1472eceb51b8ee7bffd27336c033214d
ka-son chau <ka.alex.chau@gmail.com>
2020-06-29 18:12:01 -0400

    fixed issue with user info in state not updating

--- IMDB/imdb/src/Components/MyAccountPage/MyAccountPage.js
+++ IMDB/imdb/src/Components/MyAccountPage/MyAccountPage.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MyFavorites from './MyFavorites';
 //import WatchLater from './WatchLater';
 import style from './css/MyAccountPage.module.css';
 const tokenAuth = require('../../Auth/ValidateToken');
 
 const MyAccountPage=()=>{
-    const user = tokenAuth.getUserData();
+    const [user,setUser]=useState(0);
+    useEffect(()=>{
+        const fetchUserData =async()=>{ 
+            const fetched = await tokenAuth.getUserData();
+            setUser(fetched);
+        };
+        fetchUserData();
+    },[])
     return(
         <div className={style.AccountPageContainer}>
             <MyFavorites user={user}/>
--- IMDB/imdb/src/Components/MyAccountPage/MyFavorites.js
+++ IMDB/imdb/src/Components/MyAccountPage/MyFavorites.js
@@ -3,15 +3,15 @@ import style from './css/MyFavorites.module.css';
 import axios from 'axios';
 
 const MyFavorites=(props)=>{
-    const user = useState(props.user);
+    const user = props.user;
     const [favoriteList,setFavoriteList] = useState([2,2,2]);
     useEffect(()=>{
         axios.post('/api/getfavorites',
             {
                 user:user.username
             }
-        ).then(resp=>{
-            setFavoriteList(resp.data.returnedList);
+        ).then((resp)=>{
+            setFavoriteList(resp.data);
         });
     },[user]);
 
